9019e12d838181c5ccf85e42188cc4dd5f464492
Ivana Mitrovic <ivana.mitrovic@htecgroup.com>
2024-09-01 16:29:35 +0200

    installed MUI library, created header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a>NASLOVNA</a>
+          <a>PRIJAVI SE</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
+
+export default Header;
